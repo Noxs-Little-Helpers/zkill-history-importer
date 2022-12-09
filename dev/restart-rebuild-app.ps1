7b430949be1190817928f455eda85daccfbf1a51
Nox <iamasb2@gmail.com>
2022-12-09 16:37:01 -0500

    Update dev build files and env variable format

--- dev/restart-rebuild-app.ps1
+++ dev/restart-rebuild-app.ps1
@@ -1,4 +1,4 @@
-docker build -t zkill-ws-importer ../
-docker container stop zkill-ws-importer
-docker container rm zkill-ws-importer
-docker run -itd --name zkill-ws-importer --network nlh-network zkill-ws-importer
+docker build -t zkill-ws-history ../
+docker container stop zkill-ws-history
+docker container rm zkill-ws-history
+docker run -itd --name zkill-ws-history --network nlh-network zkill-ws-history
